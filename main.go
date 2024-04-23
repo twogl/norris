bfe07a0b0a430281e0fac2694225e6529c60e2ce
Dimitar <the.ways.of.golang@gmail.com>
2024-04-24 01:11:57 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var jokes []string = []string{
+	"If you spell Chuck Norris in Scrabble, you win. Forever.",
+	"Chuck Norris breathes air ... five times a day.",
+	"Chuck Norris does not sleep. He waits.",
+	"The chief export of Chuck Norris is pain.",
+	"Chuck Norris can dribble a bowling ball.",
+	"If you want a list of Chuck Norris' enemies, just check the extinct species list.",
+	"Chuck Norris once shot an enemy plane down with his finger, by yelling,'Bang!'.",
+}
+
+func main() {
+	index := time.Now().Unix() % int64(len(jokes))
+	fmt.Println(jokes[index])
+}
